25d03fdd52bc0fd68c666acc1fe0e1b4bb9d27b8
Martina Brandan <martinabrandan38@gmail.com>
2026-05-04 20:32:42 -0300

    Problema 1

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var helper = new ProductHelper();
+
+            string resultado = helper.ObtenerEtiquetaProducto(123, "Mouse", 5000);
+
+            Console.WriteLine(resultado);
         }
     }
 }
