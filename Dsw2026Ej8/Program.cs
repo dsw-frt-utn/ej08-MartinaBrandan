74282b8a00b721235d8c32e570321e9d168da2f0
Martina Brandan <martinabrandan38@gmail.com>
2026-05-04 20:51:37 -0300

    Problema 2

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -5,11 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            var helper = new ProductHelper();
+            //ProductHelper helper = new ProductHelper();
+            //string resultado = helper.ObtenerEtiquetaProducto(123, "Mouse", 5000);
+            //Console.WriteLine(resultado);
 
-            string resultado = helper.ObtenerEtiquetaProducto(123, "Mouse", 5000);
 
-            Console.WriteLine(resultado);
+            Problema2 p2 = new Problema2();
+            Console.WriteLine(p2.CrearResumenVenta(1, "Mouse", 2, 5000));
+            Console.WriteLine(p2.CrearResumenVenta(2, "Teclado", 0, 10000));
+
+
         }
     }
 }
