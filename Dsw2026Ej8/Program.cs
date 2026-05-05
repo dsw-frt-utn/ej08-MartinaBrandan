4ea33c261fe8785be5b351aa5fbcfd6af2fd8e0d
Martina Brandan <martinabrandan38@gmail.com>
2026-05-04 21:23:45 -0300

    Problema 4

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -14,11 +14,16 @@
             //Console.WriteLine(p2.CrearResumenVenta(1, "Mouse", 2, 5000));
             //Console.WriteLine(p2.CrearResumenVenta(2, "Teclado", 0, 10000));
 
-            Product producto = new Product("Original");
-            Problema3 p3 = new Problema3();
-            string resultado = p3.CompararCopias(5, producto);
-            Console.WriteLine(resultado);
+            //Product producto = new Product("Original");
+            //Problema3 p3 = new Problema3();
+            //string resultado = p3.CompararCopias(5, producto);
+            //Console.WriteLine(resultado);
 
+            Problema4 p4 = new Problema4();
+            Console.WriteLine(p4.CalcularPromedio(8, 9, 10));     // 9
+            Console.WriteLine(p4.CalcularPromedio(8, null, 10));  // 9
+            Console.WriteLine(p4.CalcularPromedio(null, null, null)); // 0
+            Console.WriteLine(p4.CalcularPromedio(8, 20, 10));    // 9 (20 se ignora)
         }
     }
 }
