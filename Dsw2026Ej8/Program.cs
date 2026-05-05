0e760f864afbc5f75589ad0a75bed1268a5f8823
Martina Brandan <martinabrandan38@gmail.com>
2026-05-04 21:13:10 -0300

    Problema 3

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -10,10 +10,14 @@
             //Console.WriteLine(resultado);
 
 
-            Problema2 p2 = new Problema2();
-            Console.WriteLine(p2.CrearResumenVenta(1, "Mouse", 2, 5000));
-            Console.WriteLine(p2.CrearResumenVenta(2, "Teclado", 0, 10000));
+            //Problema2 p2 = new Problema2();
+            //Console.WriteLine(p2.CrearResumenVenta(1, "Mouse", 2, 5000));
+            //Console.WriteLine(p2.CrearResumenVenta(2, "Teclado", 0, 10000));
 
+            Product producto = new Product("Original");
+            Problema3 p3 = new Problema3();
+            string resultado = p3.CompararCopias(5, producto);
+            Console.WriteLine(resultado);
 
         }
     }
